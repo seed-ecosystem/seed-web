243a6b9bdfa4dd0cdcdc0201f3f9d1e22832303c
y9san9 <y9san9@gmail.com>
2025-01-12 04:32:29 +0300

    fix: Changed highlight color

--- src/modules/main/chat-list/components/chat-list-content.tsx
+++ src/modules/main/chat-list/components/chat-list-content.tsx
@@ -19,14 +19,14 @@ export function ChatListContent({chats}: ChatListProps) {
             key={chat.id}
             href={`/chat/${encodeURIComponent(chat.id)}`}
             data-active={active}
-            className={`data-[active=true]:bg-current flex flex-row items-center px-2 py-2 gap-2 whitespace-nowrap rounded-md text-sm font-medium`}>
+            className={`data-[active=true]:bg-foreground/[9%] flex flex-row items-center px-2 py-2 gap-2 whitespace-nowrap rounded-md text-sm font-medium`}>
             <Avatar className="flex justify-center items-center">
               <AvatarFallback className="text-muted-foreground">{convertTitleToAvatar(chat.title)}</AvatarFallback>
             </Avatar>
-            <div data-active={active} className="flex flex-col md:max-w-40 min-w-0 flex-1 data-[active=true]:text-white">
+            <div className="flex flex-col md:max-w-40 min-w-0 flex-1">
               <span>{chat.title}</span>
               {chat.lastMessage && (
-                <span data-active={active} className="text-muted-foreground text-xs truncate me-2 data-[active=true]:text-white/75">
+                <span className="text-muted-foreground text-xs truncate me-2">
                 {chat.lastMessage.title.split(" ")[0]}
                   :{" "}
                   {chat.lastMessage.text}
