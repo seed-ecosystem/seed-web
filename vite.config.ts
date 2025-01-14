cd44bc5f54ff4f01ce8b404203b18f959c50a10b
y9san9 <y9san9@gmail.com>
2025-01-15 01:45:02 +0300

    feat: PWA added

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnpluginTypia from '@ryoppippi/unplugin-typia/vite'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), UnpluginTypia()],
+  plugins: [
+    UnpluginTypia(),
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate'
+    })
+  ],
   base: '/seed-web/',
   resolve: {
     alias: {
